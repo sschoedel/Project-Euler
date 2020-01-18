b19f55ca484fa70ff8c720ea3eaabff3ecb7d2cf
sschoedel <sschoedel@vt.edu>
2020-01-18 02:58:59 -0500

    Flip recursion problem project euler

--- Flip Recursion.py
+++ Flip Recursion.py
@@ -1,12 +1,20 @@
 def reverseString(s):
-	if len(s) == 0:
-		return
-	temp = s[0]
-	s[0] = s[-1]
-	s[-1] = temp
-	reverseString(s[1:-1])
-	
+	a = 0
+	b = len(s) - 1
+	while a < int(len(s)/2):
+		temp = s[a]
+		s[a] = s[b]
+		s[b] = temp
+		a += 1
+		b -= 1
 
 word = ['h', 'e', 'l', 'l', 'o']
 reverseString(word)
 print(word)
+
+
+# def add_position_recur(lst, number_from=1):
+#     if not lst:
+#         return lst
+#     return [lst[0]+number_from] + add_position_recur(lst[1:], number_from+1)
+# lst = add_position_recur([1,2,3])
